d859c0ecc5ff872b0df5085369a5561638bd28f1
Sadan Iqbal <sadaniqbal@gmail.com>
2020-05-09 11:29:34 -0700

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,45 @@
-if (num < 2) {
-  return false;
-}
-for (var i = 2, l = Math.sqrt(num); i <= l; i++) {
-  if (num % i === 0) {
-    return false;
-  }
-}
-return true;
+import { Component } from "@angular/core";
 
-function range(f, t) {
-for (var i = f, r = []; i <= t; i++) {
-  r.push(i);
-}
-return r;
-}
-function primeRange(from, to) {
-var a = range(from, to),
-  r = [];
-for (var i = 0, l = a.length; i < l; i++) {
-  var v = a[i];
-  if (isPrime(v)) {
-    r.push(v);
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: [
+    "./app.component.css",
+    "/node_modules/bootstrap/dist/css/bootstrap.min.css",
+  ],
+})
+export class AppComponent {
+  title = 'Final';
+  model: any = {};
+  onSubmit() {
+    if (firstNumber/2 == 0 && secondNumber/2 ==0){
+      for()
+    }
+    if (num < 2) {
+      return false;
+    }
+    for (var i = 2, l = Math.sqrt(num); i <= l; i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  function range(f, t) {
+    for (var i = f, r = []; i <= t; i++) {
+      r.push(i);
+    }
+    return r;
+  }
+  function primeRange(from, to) {
+    var a = range(from, to),
+      r = [];
+    for (var i = 0, l = a.length; i < l; i++) {
+      var v = a[i];
+      if (isPrime(v)) {
+        r.push(v);
+      }
+    }
+    return r;
   }
 }
-return r;
